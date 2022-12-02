3a037f5d72fda8f58bae64e16e1ed13a15c0f2da
Florent LOTTHÉ <florent.lotthe@decathlon.com>
2022-12-02 20:08:26 +0100

    fix: disabled state

--- Sources/VitaminSwiftUI/Components/TextField/VitaminTextFieldModifier.swift
+++ Sources/VitaminSwiftUI/Components/TextField/VitaminTextFieldModifier.swift
@@ -68,6 +68,7 @@ public struct VitaminTextFieldModifier: ViewModifier {
             }
             makeUnderTextView()
         }
+        .disabled(state == .disabled)
         .padding()
     }
 
