470a4e23accfba4f69e5a7f103faf231295da5a3
Jeffrey Macko <jeffrey.macko@decathlon.com>
2023-10-13 16:20:23 +0200

    Update tests

--- Showcase/ShowcaseSnapshotTests/ShowcaseSnapshotSwiftUITests.swift
+++ Showcase/ShowcaseSnapshotTests/ShowcaseSnapshotSwiftUITests.swift
@@ -14,7 +14,22 @@ import PreviewSnapshotsTesting
 
 @available(iOS 13, *)
 final class ShowcaseSnapshotSwiftUITests: VitaminTestCase {
-  
+  func testIconsTemplateMode() throws {
+    assertSnapshot(
+      matching: Image.Vitamix.searchLine
+        .frame(width: 100, height: 100)
+        .foregroundStyle(.red),
+      as: .image(perceptualPrecision: perceptualPrecision),
+      named: "red"
+    )
+    assertSnapshot(
+      matching: Image.Vitamix.searchLine
+        .frame(width: 100, height: 100),
+      as: .image(perceptualPrecision: perceptualPrecision),
+      named: "black"
+    )
+  }
+
   func testButtonsView() throws {
     ButtonsView_Previews.snapshots.assertSnapshots(as: .image(perceptualPrecision: perceptualPrecision))
   }
